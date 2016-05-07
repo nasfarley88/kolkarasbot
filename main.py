98afdd7db16b510dc3243843e17182c92e580eb6
Nathanael Farley <nasfarley88@gmail.com>
2016-05-07 17:12:51 +0100

    Restructure to use SpeakerBot.

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ from telepot.namedtuple import ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboar
 from telepot.namedtuple import InlineKeyboardMarkup, InlineKeyboardButton
 from telepot.namedtuple import InlineQueryResultArticle, InlineQueryResultPhoto, InputTextMessageContent
 
+import dataset
+
 import dice
 
 import re
@@ -58,33 +60,86 @@ help_message = """~~~ ODIN TRANSMISSION ~~~
 ~~~ END ODIN TRANSMISSION ~~~
 """
 
-async def on_chat_message(msg):
-    content_type, chat_type, chat_id = telepot.glance(msg)
-    print('Chat:', content_type, chat_type, chat_id)
 
-    if content_type != 'text':
-        return
+class KolkarasBot(telepot.async.SpeakerBot):
+    async def on_chat_message(self, msg):
+        content_type, chat_type, chat_id = telepot.glance(msg)
+        print('Chat:', content_type, chat_type, chat_id)
 
-    parsed_text = await parse_command(msg['text'])
-    if not parsed_text:
-        return
+        # Shove every message in the microphone
+        self.mic.send(msg)
 
-    if "/help" == parsed_text[0]:
-        await bot.sendMessage(chat_id, help_message)
+        if content_type != 'text':
+            return
 
-    if "/latest" == parsed_text[0]:
-        await bot.sendMessage(
+        parsed_text = await parse_command(msg['text'])
+        if not parsed_text:
+            return
+
+        if "/help" == parsed_text[0]:
+            await self.sendMessage(chat_id, help_message)
+
+        if "/latest" == parsed_text[0]:
+            await self.sendMessage(
+                chat_id,
+                await odin_transmission("WARNING: CURRENT NEWS FEEDS ARE NOT OPERATIONAL."))
+
+        if "/roll" == parsed_text[0]:
+            await self.sendMessage(
+                chat_id,
+                await roll_the_dice(
+                    ''.join(parsed_text[1] if parsed_text[1] else "3d6"
+                    )),
+                parse_mode="Markdown")
+
+        if "/newlore" == parsed_text[0]:
+            await self.enter_lore(msg)
+
+        if "/lore" == parsed_text[0]:
+            await self.search_lore(msg)
+
+    async def create_listener(self, chat_id, **kwargs):
+        """Create listener and wait for response. """
+        listener = super(KolkarasBot, self).create_listener()
+        listener.capture(chat__id=chat_id, **kwargs)
+        return listener
+
+    async def enter_lore(self, msg):
+        """Enters lore into database."""
+        content_type, chat_type, chat_id = telepot.glance(msg)
+        listener = await self.create_listener(chat_id)
+        await self.sendMessage(chat_id, "Enter entry name:")
+        entry_name = (await listener.wait())['text']
+        await self.sendMessage(chat_id, "Enter entry text:")
+        entry_text = (await listener.wait())['text']
+
+        with dataset.connect() as db:
+            table = db["lore_arcadia"]
+            table.insert(dict(
+                name=entry_name,
+                text=entry_text,))
+
+        await self.sendMessage(chat_id, "Entry added.")
+
+    async def search_lore(self, msg):
+        """Search lore."""
+        content_type, chat_type, chat_id = telepot.glance(msg)
+        listener = await self.create_listener(chat_id)
+
+        await self.sendMessage(chat_id, "Which entry would you like?")
+        entry_name = (await listener.wait())['text']
+        with dataset.connect() as db:
+            table = db['lore_arcadia']
+            entry = table.find_one(
+                name=entry_name)
+
+        await self.sendMessage(
             chat_id,
-            await odin_transmission("WARNING: CURRENT NEWS FEEDS ARE NOT OPERATIONAL."))
+            "{}\n\n{}".format(
+                entry['name'],
+                entry['text']))
 
-    if "/roll" == parsed_text[0]:
-        await bot.sendMessage(
-            chat_id,
-            await roll_the_dice(
-                ''.join(parsed_text[1] if parsed_text[1] else "3d6"
-                )),
-            parse_mode="Markdown")
-    # command = msg['text'][-1:].lower()
+
 
     # if command == 'c':
     #     markup = ReplyKeyboardMarkup(keyboard=[
@@ -166,15 +221,24 @@ async def on_chat_message(msg):
 
 TOKEN = sys.argv[1]  # get token from command-line
 
-bot = telepot.async.Bot(TOKEN)
-answerer = telepot.async.helper.Answerer(bot)
-
+bot = KolkarasBot(TOKEN)
 loop = asyncio.get_event_loop()
-loop.create_task(bot.message_loop({'chat': on_chat_message,
-                                   # 'callback_query': on_callback_query,
-                                   # 'inline_query': on_inline_query,
-                                   # 'chosen_inline_result': on_chosen_inline_result,
-}))
+
+loop.create_task(bot.message_loop())
 print('Listening ...')
 
 loop.run_forever()
+# TOKEN = sys.argv[1]  # get token from command-line
+
+# bot = telepot.async.SpeakerBot(TOKEN)
+# answerer = telepot.async.helper.Answerer(bot)
+
+# loop = asyncio.get_event_loop()
+# loop.create_task(bot.message_loop({'chat': on_chat_message,
+#                                    # 'callback_query': on_callback_query,
+#                                    # 'inline_query': on_inline_query,
+#                                    # 'chosen_inline_result': on_chosen_inline_result,
+# }))
+# print('Listening ...')
+
+# loop.run_forever()
