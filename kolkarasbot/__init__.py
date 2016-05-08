3eb36705eb8b20613d4ae906ee4e50e5fb9af75b
Nathanael Farley <nasfarley88@gmail.com>
2016-05-09 00:27:39 +0100

    More sensitive lore search with fuzzywuzzy.

--- kolkarasbot/__init__.py
+++ kolkarasbot/__init__.py
@@ -3,11 +3,25 @@ import sys
 import asyncio
 import telepot
 import telepot.async
+from fuzzywuzzy import process
+import re
 
-import dataset
 from urllib.request import urlopen
 
 from . import utils
+
+from fuzzywuzzy.fuzz import WRatio, ratio
+
+def custom_match(s1, s2):
+    """A custom matching function which weights strings which match ratio.
+
+    Check the code."""
+
+    main_match = 100*WRatio(s1, s2)
+    ratio_match = ratio(s1, s2)
+
+    return main_match + ratio_match
+
 
 class KolkarasBot(telepot.async.SpeakerBot):
     async def on_chat_message(self, msg):
@@ -34,7 +48,8 @@ class KolkarasBot(telepot.async.SpeakerBot):
         if "/latest" == parsed_text[0]:
             await self.sendMessage(
                 chat_id,
-                await odin_transmission("WARNING: CURRENT NEWS FEEDS ARE NOT OPERATIONAL."))
+                await odin_transmission(
+                    "WARNING: CURRENT NEWS FEEDS ARE NOT OPERATIONAL."))
 
         if "/roll" == parsed_text[0]:
             await self.sendMessage(
@@ -43,9 +58,6 @@ class KolkarasBot(telepot.async.SpeakerBot):
                     ''.join(parsed_text[1] if parsed_text[1] else "3d6"
                     )),
                 parse_mode="Markdown")
-
-        # if "/newlore" == parsed_text[0]:
-        #     await self.enter_lore(msg)
 
         if "/lore" == parsed_text[0]:
             await self.search_lore(msg)
@@ -66,22 +78,6 @@ class KolkarasBot(telepot.async.SpeakerBot):
             chat_id,
             "http://{}:4567/Home".format(ip)
         )
-    # async def enter_lore(self, msg):
-    #     """Enters lore into database."""
-    #     content_type, chat_type, chat_id = telepot.glance(msg)
-    #     listener = await self.create_listener(chat_id)
-    #     await self.sendMessage(chat_id, "Enter entry name:")
-    #     entry_name = (await listener.wait())['text']
-    #     await self.sendMessage(chat_id, "Enter entry text:")
-    #     entry_text = (await listener.wait())['text']
-
-    #     with dataset.connect() as db:
-    #         table = db["lore_arcadia"]
-    #         table.insert(dict(
-    #             name=entry_name,
-    #             text=entry_text,))
-
-    #     await self.sendMessage(chat_id, "Entry added.")
 
     async def search_lore(self, msg):
         """Search lore."""
@@ -90,22 +86,20 @@ class KolkarasBot(telepot.async.SpeakerBot):
 
         await self.sendMessage(chat_id, "Which entry would you like?")
         entry_name = (await listener.wait())['text']
-        path = "wiki/lore/{}.md".format(entry_name.replace(' ', '-'))
-        if os.path.exists(path):
-            with open(path) as file:
-                entry = {"name": entry_name,
-                         "text": file.read()}
-            # with dataset.connect() as db:
-            #     table = db['lore_arcadia']
-            #     entry = table.find_one(
-            #         name=entry_name)
 
+        choices = os.listdir("wiki/lore")
+        with open(
+                "wiki/lore/{}".format(
+                    process.extractOne(
+                        entry_name,
+                        choices,
+                        scorer=custom_match)[0])) as data_file:
+            data = re.sub(r"\[(.*?)\]\(.*?\)", r"*\1*", data_file.read())
+            data = re.sub(r"\[\[(.*?)\]\]", r"*\1*", data)
             await self.sendMessage(
                 chat_id,
-                "{}\n\n{}".format(
-                    entry['name'],
-                    entry['text']))
-        else:
-            await self.sendMessage(
-                chat_id,
-            "No entry found")
+                await utils.odin_transmission(
+                    "Entry matching: {}\n\n{}".format(
+                        data_file.name.split('/')[-1].replace('.md', ''),
+                        data)),
+                parse_mode="Markdown")
